4e9da03c9281b1b53a5951d72b2cf0074e3b8485
BunsExynos <himanshuakela@gmail.com>
2018-10-17 07:09:59 +0530

    a5xelte : move ril init to common ramdisk

--- configs/product/ril.mk
+++ configs/product/ril.mk
@@ -34,5 +34,4 @@ PRODUCT_PACKAGES += \
 
 # cpboot-daemon for modem
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/radio/cbd:system/bin/cbd \
-    device/samsung/exynos7580-common/configs/init/rild.rc:system/vendor/etc/init/rild.rc
+    $(LOCAL_PATH)/configs/radio/cbd:system/bin/cbd
