0a343ad592ad6b84b1abea9a485cffb06aa39213
BunsExynos <himanshuakela@gmail.com>
2018-10-17 06:47:02 +0530

    Revert "a5xelte : don't binderize gnss"
    This reverts commit db79d6d41380bc42cfc7a8ec3db92891026a535b.

--- configs/product/gps.mk
+++ configs/product/gps.mk
@@ -24,4 +24,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
