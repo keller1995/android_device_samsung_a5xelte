da3c824bb25af084f6128cadcc761549c445a4a9
BunsExynos <himanshuakela@gmail.com>
2018-11-23 07:18:13 +0530

    a5xele : remove nfc wrapper

--- configs/product/nfc.mk
+++ configs/product/nfc.mk
@@ -30,6 +30,4 @@ PRODUCT_PACKAGES += \
 	NfcNci \
 	Tag \
 	com.android.nfc_extras \
-	android.hardware.nfc@1.0-impl \
-	android.hardware.nfc@1.0-service \
-	nfc_nci.universal7580
+	android.hardware.nfc@1.0-impl
